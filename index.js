157f03ff6f440e5121000d0ab444a72fabdac31e
Karalyn Ferrari <lifesmygift@gmail.com>
2018-02-22 02:01:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 const app = "I don't do much."
-destructivelyAppendKitten(name)
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){ 
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens;
+}
+
+function appendKitten(name){
+  return [...kittens,name];
+}
+
+function prependKitten(name){
+  return [kittens,...array];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,kittens.length-1);
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
