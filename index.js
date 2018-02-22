2792a60a29788b293b27f9c42b86aff4e0849550
Karalyn Ferrari <lifesmygift@gmail.com>
2018-02-22 02:02:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ function destructivelyRemoveLastKitten(){
   return kittens;
 }
 
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens;
+}
+
 function appendKitten(name){
   return [...kittens,name];
 }
